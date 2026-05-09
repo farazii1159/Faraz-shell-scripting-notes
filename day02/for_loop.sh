21489e5006573387de581bebbad0c924ef0f29fa
Faraz Shabbir <farazii1159@gmail.com>
2026-05-09 11:40:13 +0500

    Update for_loop.sh

--- day02/for_loop.sh
+++ day02/for_loop.sh
@@ -15,3 +15,10 @@ do
 done
 
 
+Ye ek simple Bash shell script hai jo `for loop` aur command line arguments ka use karke multiple folders automatically create karti
+hai. Script `#!/bin/bash` se start hoti hai jo batata hai ke code Bash shell me run hoga. `<< comment ... comment` block multi-line 
+comment ke liye use hua hai jahan `$1`, `$2`, aur `$3` arguments ka purpose explain kiya gaya hai. Yahan `$1` folder name ka base 
+hai, `$2` starting range hai aur `$3` ending range hai. `for (( num=$2 ; num<=$3; num++ ))` loop start range se end range tak chalta 
+hai aur har iteration me `mkdir "$1$num"` command ek naya folder create karti hai. Agar script ko `./script.sh project 1 5` se run
+karein to ye `project1`, `project2`, `project3`, `project4`, aur `project5` naam ke folders automatically create kar degi. Ye script
+Bash scripting me loops, variables aur automation ka basic practical example hai.
